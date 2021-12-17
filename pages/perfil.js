04202f636bb711b89a4272ff3f7a9519fa6a36cb
Oscar Ramos <64324454+lacroix021@users.noreply.github.com>
2021-12-17 12:04:20 -0500

    Initial commit

---
+++ pages/perfil.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import Layout from '../components/layout'
+import AccessDenied from '../components/access-denied'
+import styles from "./profile.module.css"
+
+export default function Page() {
+    const { data: session, status } = useSession()
+    const loading = status === 'loading'
+    const [ content , setContent ] = useState()
+
+    if(session){const {user} = session;}
+
+    if (!session) { return  <Layout><AccessDenied/></Layout> }
+
+    console.log(status);
+  return (
+    <Layout>
+      <h1>Pagina de perfil de {user.name}</h1>
+      <img src={user.image} className={styles.profilePhoto}></img>
+      <h3>
+          y este es su correo: {user.email}
+      </h3>
+      <p><strong>{content || "\u00a0"}</strong></p>
+    </Layout>
+  )
+}
